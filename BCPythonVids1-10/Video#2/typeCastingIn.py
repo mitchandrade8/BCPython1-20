9f3e804074005ce14b22ef771603d51253e2c6ac
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-24 11:19:56 -0700

    --Start: of type casting in Python

---
+++ BCPythonVids1-10/Video#2/typeCastingIn.py
@@ -0,0 +1,6 @@
+
+# Key Note:
+    # typecasting = The process of converting a value of one data type to another
+    #               (string, integer, float, boolean)
+    #               Explicit vs Implicit
+
