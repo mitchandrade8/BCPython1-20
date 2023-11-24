ade89b091085dc276751f7946b4b4680e3a80359
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-23 22:01:03 -0700

    --Boolean and print(f) overview in Python

--- BCPythonVids1-10/Video#1/4MainDataTypesIn.py
+++ BCPythonVids1-10/Video#1/4MainDataTypesIn.py
@@ -24,13 +24,20 @@
 
 # String
 
-name = "Mitch"
-food = "Chiptole"
-email = "mitchandrade@gmail.com"
+# name = "Mitch"
+# food = "Chiptole"
+# email = "mitchandrade@gmail.com"
 
-print(f"Hello {name}")
-print(f"You like {food}")
-print(f"Your email is: {email}")
+# print(f"Hello {name}")
+# print(f"You like {food}")
+# print(f"Your email is: {email}")
 
 # Boolean
 
+online = True
+for_sale = False
+running = True
+
+print(f"Are you online?: {online}")
+print(f"Is the item for sale?: {for_sale}")
+print(f"Game running: {running}")
