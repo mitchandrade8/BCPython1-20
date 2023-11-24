a7742bc09e428774ca49581158d212dddc28e42d
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-23 21:36:28 -0700

    print(f) strings in python

--- BCPythonVids1-10/Video#1/VariablesAndDataTypesIn.py
+++ BCPythonVids1-10/Video#1/VariablesAndDataTypesIn.py
@@ -7,4 +7,7 @@ age = 21
 
 #print(age)
 
-print("You are \(age) yeats old!")
+print("You are " + str(age) + " years old!")
+print("You are ", age, " years old!" )
+
+print(f"You are {age} years old")
