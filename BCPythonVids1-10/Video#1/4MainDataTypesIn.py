f01e947d79bb1d48b18302ac93e98a35e4881731
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-23 21:51:01 -0700

    --Overview of Int and Floats complete

--- BCPythonVids1-10/Video#1/4MainDataTypesIn.py
+++ BCPythonVids1-10/Video#1/4MainDataTypesIn.py
@@ -1,8 +1,30 @@
 
+
+
 # Integer
+
+# age = 21
+# players = 2
+# quantity = 5
+
+# print(f"You are {age} years old")
+# print(f"There are {players} players online")
+# print(f"You would like to buy {quantity} items")
+
 
 # Float
 
+gpa = 3.2
+distance = 2.5
+price = 10.99
+
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance}Km")
+print(f"The price is ${price}")
+
 # String
 
+
+
 # Boolean
+
