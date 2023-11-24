c8fbdae56816ca134918525caf0d9258242a4187
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-23 20:19:44 -0700

    string concatanation in Python

---
+++ BCPythonVids1-10/Video#1/VariablesAndDataTypesIn.py
@@ -0,0 +1,10 @@
+
+# Key Notes:
+    # Variable = a reusable container for storing a value 
+    #   a variable behaves as it it were the value it contains
+
+age = 21
+
+#print(age)
+
+print("You are \(age) yeats old!")
